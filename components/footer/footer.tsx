5e99df989c6f6538c40a83b2f4938e90245aae71
1000VIA <milvampa1@gmail.com>
2021-06-29 11:20:21 -0500

    some components

---
+++ components/footer/footer.tsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <footer className="border-t-2 w-full flex items-center justify-center h-footer bottom-0 fixed">
+      <p className="text-white font-bold">&copy; 1000VIA 2021</p>
+    </footer>
+  );
+};
+export default Footer;
